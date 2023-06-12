df94e9575d4c063c1f2fd6a7b05b6fcabeea13ff
Andre Maurell <andremaurell3@furg.br>
2023-06-12 15:47:34 -0300

    add clusters

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+
+if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} is running`);
+
+  // Crie um worker para cada CPU disponível
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died. Creating a new worker.`);
+    cluster.fork();
+  });
+
+  // Escute eventos de fork de novos workers
+  cluster.on('fork', (worker) => {
+    console.log(`Worker ${worker.process.pid} was forked`);
+  });
+
+  // Escute eventos de saída de workers
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died with code ${code} and signal ${signal}`);
+    cluster.fork()
+  });
+
+} else {
 const app = require('express')()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server, {cors: {origin: '*'}})
@@ -26,3 +54,4 @@ io.on('connection', socket => {
 
 server.listen(PORT, () => 
 console.log(`Worker ${process.pid} is running on port ${PORT}`))
+}
