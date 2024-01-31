685122ead09f38a81c0e7f2c3c65897bf8ce408d
andremaurell <andremaurell3@gmail.com>
2024-01-31 10:11:02 -0300

    vercel

--- index.js
+++ index.js
@@ -24,12 +24,8 @@ app.use(router)
 config()
 
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: parseInt(process.env.PGPORT || '5432', 10),
-});
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+})
 
 
 io.on('connection', (socket) => {
